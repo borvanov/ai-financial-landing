77baa4a68e5a0fff2b461beceaa03f1506c4f279
Anton Borvanov <anton.borvanov@gmail.com>
2023-03-11 14:03:40 +0300

    FAQ: questions view added

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,11 +1,27 @@
 import Head from "next/head";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "@/styles/faq.module.css";
 
 import { Header } from "@/src/shared/components/header/header.component";
 import { Footer } from "@/src/shared/components/footer/footer.component";
 
+const QUESTIONS = [
+  {
+    question: "Is there a free",
+    answer: "Yes",
+  },
+  {
+    question: "Is there a free 2",
+    answer: "Yes",
+  },
+];
+
 export default function FAQ() {
+  const [openedQuestion, setOpenedQuestion] = useState<string | null>(null);
+
   return (
     <>
       <Head>
@@ -18,8 +34,44 @@ export default function FAQ() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={styles.container}>
-        
+      <main className={`${styles.container} content`}>
+        <span className="block-subheader">FAQs</span>
+        <h2 className="block-header">Frequently Asked Questions</h2>
+        <p className={styles.description}>
+          Have questions? We are here to help.
+        </p>
+
+        <div className={styles.questionsContainer}>
+          {QUESTIONS.map(({ question, answer }, index, array) => (
+            <>
+              <div
+                className={styles.questionContainer}
+                onClick={() => {
+                  if (question === openedQuestion) {
+                    setOpenedQuestion(null);
+                  } else {
+                    setOpenedQuestion(question);
+                  }
+                }}
+                key={question}
+              >
+                <span className={styles.questionText}>{question}</span>
+                <FontAwesomeIcon
+                  className={styles.plusIcon}
+                  icon={
+                    question === openedQuestion ? faMinusCircle : faPlusCircle
+                  }
+                />
+              </div>
+
+              {question === openedQuestion ? <p>{answer}</p> : null}
+
+              {index !== array.length - 1 ? (
+                <div className={styles.divider} />
+              ) : null}
+            </>
+          ))}
+        </div>
       </main>
       <Footer />
     </>
