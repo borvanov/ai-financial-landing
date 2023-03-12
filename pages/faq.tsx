dd1609c4c0aa01139a3c2a685dee8568a322748c
Anton Borvanov <anton.borvanov@gmail.com>
2023-03-12 17:03:06 +0300

    Support page added

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -7,6 +7,7 @@ import styles from "@/styles/faq.module.css";
 
 import { Header } from "@/src/shared/components/header/header.component";
 import { Footer } from "@/src/shared/components/footer/footer.component";
+import Link from "next/link";
 
 const QUESTIONS = [
   {
@@ -34,44 +35,57 @@ export default function FAQ() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={`${styles.container} content`}>
-        <span className="block-subheader">FAQs</span>
-        <h2 className="block-header">Frequently Asked Questions</h2>
-        <p className={styles.description}>
-          Have questions? We are here to help.
-        </p>
+      <main className={styles.container}>
+        <section className={`${styles.contentContainer} content`}>
+          <span className="block-subheader">FAQs</span>
+          <h2 className="block-header">Frequently Asked Questions</h2>
+          <p className={styles.description}>
+            Have questions? We are here to help.
+          </p>
 
-        <div className={styles.questionsContainer}>
-          {QUESTIONS.map(({ question, answer }, index, array) => (
-            <>
-              <div
-                className={styles.questionContainer}
-                onClick={() => {
-                  if (question === openedQuestion) {
-                    setOpenedQuestion(null);
-                  } else {
-                    setOpenedQuestion(question);
-                  }
-                }}
-                key={question}
-              >
-                <span className={styles.questionText}>{question}</span>
-                <FontAwesomeIcon
-                  className={styles.plusIcon}
-                  icon={
-                    question === openedQuestion ? faMinusCircle : faPlusCircle
-                  }
-                />
-              </div>
+          <div className={styles.questionsContainer}>
+            {QUESTIONS.map(({ question, answer }, index, array) => (
+              <>
+                <div
+                  className={styles.questionContainer}
+                  onClick={() => {
+                    if (question === openedQuestion) {
+                      setOpenedQuestion(null);
+                    } else {
+                      setOpenedQuestion(question);
+                    }
+                  }}
+                  key={question}
+                >
+                  <span className={styles.questionText}>{question}</span>
+                  <FontAwesomeIcon
+                    className={styles.plusIcon}
+                    icon={
+                      question === openedQuestion ? faMinusCircle : faPlusCircle
+                    }
+                  />
+                </div>
 
-              {question === openedQuestion ? <p>{answer}</p> : null}
+                {question === openedQuestion ? <p>{answer}</p> : null}
 
-              {index !== array.length - 1 ? (
-                <div className={styles.divider} />
-              ) : null}
-            </>
-          ))}
-        </div>
+                {index !== array.length - 1 ? (
+                  <div className={styles.divider} />
+                ) : null}
+              </>
+            ))}
+          </div>
+
+          <div className={styles.supportContainer}>
+            <h2>Still have questions?</h2>
+            <p className={styles.supportDescriptionText}>
+              Can not find the answer you are looking for? We are ready to help
+              you. Please write a message to our support.
+            </p>
+            <Link href="/support" className="button primary">
+              Chat with us
+            </Link>
+          </div>
+        </section>
       </main>
       <Footer />
     </>
--- pages/support.tsx
+++ pages/support.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Form from "react-bootstrap/Form";
 
 import styles from "@/styles/faq.module.css";
 
@@ -6,6 +7,13 @@ import { Header } from "@/src/shared/components/header/header.component";
 import { Footer } from "@/src/shared/components/footer/footer.component";
 
 export default function Support() {
+  const supportOptions = [
+    "Technical support",
+    "Advertisement",
+    "Functional support",
+    "Other",
+  ];
+
   return (
     <>
       <Head>
@@ -15,8 +23,38 @@ export default function Support() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={styles.container}></main>
+      <main className={styles.container}>
+        <section className="content">
+          <span className="block-subheader">Chat with us</span>
+          <h2 className="block-header">Support</h2>
 
+          <Form>
+            <Form.Group className="mb-3" controlId="topic">
+              <Form.Label>Topic</Form.Label>
+              <Form.Select defaultValue={supportOptions[0]}>
+                {supportOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="message">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                type="text"
+                aria-multiline
+                placeholder="Enter your message"
+              />
+            </Form.Group>
+
+            <button className="primary" type="submit">
+              Send
+            </button>
+          </Form>
+        </section>
+      </main>
       <Footer />
     </>
   );
